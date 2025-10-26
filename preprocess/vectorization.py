de0b1fa98390bce0eef700e692072fda0db65e0c
Ahmed <gonimeahmed73@gmail.com>
2025-10-26 17:01:17 +0300

    Update file paths for vectorizer model

--- preprocess/vectorization.py
+++ preprocess/vectorization.py
@@ -9,11 +9,11 @@ class Vectorizer:
     def fit_on_df(self, df):
         self.tfidf.fit(df['Resume'])
         Required = self.tfidf.transform(df['Resume'])
-        jb.dump(self.tfidf, r'C:\I will prepare my self to destroy the world\Programming\python program\MY Projects\NLP Projects\Resume analyser\models\vectorizer.pkl')
+        jb.dump(self.tfidf, r'\models\vectorizer.pkl')
         return Required
     
     def load_vectorizer(self, txt):
-        self.tfidf = jb.load(r'C:\I will prepare my self to destroy the world\Programming\python program\MY Projects\NLP Projects\Resume analyser\models\vectorizer.pkl')
+        self.tfidf = jb.load(r'\models\vectorizer.pkl')
         txt = self.tfidf.transform([txt])
         txt = txt.toarray()
         return txt
