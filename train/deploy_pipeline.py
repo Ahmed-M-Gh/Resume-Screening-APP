5f42bdd828aa460e6485e6751fb241de99638a46
Ahmed <gonimeahmed73@gmail.com>
2025-10-26 16:58:54 +0300

    Update model and label loading paths

--- train/deploy_pipeline.py
+++ train/deploy_pipeline.py
@@ -11,10 +11,10 @@ class Dep_pipeline:
         X = vec.load_vectorizer(txt)
         
         
-        model = jb.load(r'C:\I will prepare my self to destroy the world\Programming\python program\MY Projects\NLP Projects\Resume analyser\models\KNC model.pkl')
+        model = jb.load(r'models\KNC model.pkl')
         predict = model.predict(X)
         
-        lb = jb.load(r'C:\I will prepare my self to destroy the world\Programming\python program\MY Projects\NLP Projects\Resume analyser\models\encoding labels.pkl')
+        lb = jb.load(r'models\encoding labels.pkl')
         prediction_name = lb.inverse_transform(predict)
         return prediction_name
         
